da7ac7c61bf7b4da08481875dacb1b0e96d0e78e
dandyday <dandyday31@gmail.com>
2024-03-11 16:30:07 +0900

    Feat: Google Login

--- script.js
+++ script.js
@@ -1,136 +1,261 @@
-let nav = 0; 			// current month - 1
-let clicked = null;		// clicked date
-let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
+let nav = 0; // current month - 1
+let clicked = null; // clicked date
+let events = [];
+let year, month, date;
 
-const calendar = document.getElementById('calendar');
-const newEventModal = document.getElementById('newEventModal');
-const deleteEventModal = document.getElementById('deleteEventModal');
-const backDrop = document.getElementById('modalBackDrop');
-const eventTitleInput = document.getElementById('eventTitleInput');
-const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const calendar = document.getElementById("calendar");
+const newEventModal = document.getElementById("newEventModal");
+const deleteEventModal = document.getElementById("deleteEventModal");
+const backDrop = document.getElementById("modalBackDrop");
+const eventTitleInput = document.getElementById("eventTitleInput");
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const CLIENT_ID = "";
+const API_KEY = "";
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+const SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+
+// Discovery doc URL for APIs used by the quickstart
+const DISCOVERY_DOC =
+  "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest";
+
+/**
+ * Callback after Google Identity Services are loaded.
+ */
+function gisLoaded() {
+  tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: CLIENT_ID,
+    scope: SCOPES,
+    callback: "", // defined later
+  });
+  gisInited = true;
+  // maybeEnableButtons();
+}
+
+/**
+ * Callback after api.js is loaded.
+ */
+function gapiLoaded() {
+  gapi.load("client", initializeGapiClient);
+}
+
+/**
+ * Callback after the API client is loaded. Loads the
+ * discovery doc to initialize the API.
+ */
+async function initializeGapiClient() {
+  await gapi.client.init({
+    apiKey: API_KEY,
+    discoveryDocs: [DISCOVERY_DOC],
+  });
+  gapiInited = true;
+  // maybeEnableButtons();
+}
 
 function openModal(date) {
-	clicked = date;
+  clicked = date;
 
-	const eventForDay = events.find(e => e.date == clicked);
+  const eventForDay = events.find((e) => e.date == clicked);
 
-	if (eventForDay) {
-		document.getElementById('eventText').innerText = eventForDay.title;
-		deleteEventModal.style.display = 'block';
-	} else {
-		newEventModal.style.display = 'block';
-	}
-	backDrop.style.display = 'block';
+  if (eventForDay) {
+    document.getElementById("eventText").innerText = eventForDay.title;
+    deleteEventModal.style.display = "block";
+  } else {
+    newEventModal.style.display = "block";
+  }
+  backDrop.style.display = "block";
 }
 
 function load() {
-	const dt = new Date();
+  console.log(events);
+  events = [];
+  const dt = new Date();
 
-	if (nav !== 0) {
-		dt.setMonth(new Date().getMonth() + nav);
-	}
+  if (nav !== 0) {
+    dt.setMonth(new Date().getMonth() + nav);
+  }
 
-	const day = dt.getDate();
-	const month = dt.getMonth();
-	const year = dt.getFullYear();
+  day = dt.getDate();
+  month = dt.getMonth();
+  year = dt.getFullYear();
+  listUpcomingEvents().then(loadEventsToCalendar);
+}
 
-	const firstDayOfMonth = new Date(year, month, 1);
-	const daysInMonth = new Date(year, month + 1, 0).getDate();
-	const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
-		weekday: 'long',
-		year: 'numeric',
-		month: 'numeric',
-		day: 'numeric',
-	});
+function loadEventsToCalendar() {
+  const dt = new Date();
+  const firstDayOfMonth = new Date(year, month, 1);
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const dateString = firstDayOfMonth.toLocaleDateString("en-us", {
+    weekday: "long",
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  });
 
-	const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+  const paddingDays = weekdays.indexOf(dateString.split(", ")[0]);
 
-	document.getElementById('monthDisplay').innerText = `${dt.toLocaleDateString('en-us', {month: 'long'})} ${year}`;
+  document.getElementById("monthDisplay").innerText = `${dt.toLocaleDateString(
+    "en-us",
+    { month: "long" }
+  )} ${year}`;
 
-	calendar.innerHTML = '';
+  calendar.innerHTML = "";
 
-	for(let i = 1; i <= paddingDays + daysInMonth; i++) {
-		const daySquare = document.createElement('div');
-		daySquare.classList.add('day');
+  for (let i = 1; i <= paddingDays + daysInMonth; i++) {
+    const daySquare = document.createElement("div");
+    daySquare.classList.add("day");
 
-		const dayString = `${month + 1}/${i - paddingDays}/${year}`;
-		if (i > paddingDays) {
-			daySquare.innerText = i - paddingDays;
-			
-			const eventForDay = events.find(e => e.date === dayString);
+    const dayString = `${month + 1}/${i - paddingDays}/${year}`;
+    if (i > paddingDays) {
+      daySquare.innerText = i - paddingDays;
 
-			if (i - paddingDays === day && nav === 0) {
-				daySquare.id = 'currentDay';
-			}
+      const eventsForDay = events.filter((e) => e.date === dayString);
 
-			if (eventForDay) {
-				const eventDiv = document.createElement('div');
-				eventDiv.classList.add('event');
-				eventDiv.innerText = eventForDay.title;
-				daySquare.appendChild(eventDiv);
-			}
-			
-			daySquare.addEventListener('click', () => openModal(`${month + 1}/${i - paddingDays}/${year}`));
+      if (i - paddingDays === day && nav === 0) {
+        daySquare.id = "currentDay";
+      }
 
+      eventsForDay.forEach((eventForDay) => {
+        const eventDiv = document.createElement("div");
+        eventDiv.classList.add("event");
+        eventDiv.innerText = eventForDay.title;
+        daySquare.appendChild(eventDiv);
+      });
 
+      daySquare.addEventListener("click", () =>
+        openModal(`${month + 1}/${i - paddingDays}/${year}`)
+      );
+    } else {
+      daySquare.classList.add("padding");
+    }
 
-		} else {
-			daySquare.classList.add('padding');
-		}
-
-		calendar.appendChild(daySquare);
-
-	}
-	console.log(paddingDays);
-	console.log(dateString);
+    calendar.appendChild(daySquare);
+  }
+  console.log(paddingDays);
+  console.log(dateString);
 }
 
 function saveEvent() {
-	if (eventTitleInput.value) {
-		eventTitleInput.classList.remove('error');
-		events.push({
-			date: clicked,
-			title: eventTitleInput.value,
-		})
+  if (eventTitleInput.value) {
+    eventTitleInput.classList.remove("error");
+    events.push({
+      date: clicked,
+      title: eventTitleInput.value,
+    });
 
-		localStorage.setItem('events', JSON.stringify(events));
-		closeModal();
-	} else {
-		eventTitleInput.classList.add('error');
-	}
+    localStorage.setItem("events", JSON.stringify(events));
+    closeModal();
+  } else {
+    eventTitleInput.classList.add("error");
+  }
 }
 
 function closeModal() {
-	eventTitleInput.classList.remove('error');
-	newEventModal.style.display = 'none';
-	deleteEventModal.style.display = 'none';
-	backDrop.style.display = 'none';
-	eventTitleInput.value = '';
-	clicked = null;
-	load();
+  eventTitleInput.classList.remove("error");
+  newEventModal.style.display = "none";
+  deleteEventModal.style.display = "none";
+  backDrop.style.display = "none";
+  eventTitleInput.value = "";
+  clicked = null;
+  load();
 }
 
 function deleteEvent() {
-	events = events.filter(e => e.date !== clicked);
-	localStorage.setItem('events', JSON.stringify(events));
-	closeModal();
+  events = events.filter((e) => e.date !== clicked);
+  localStorage.setItem("events", JSON.stringify(events));
+  closeModal();
+}
+
+let tokenClient;
+function handleAuthClick() {
+  tokenClient.callback = async (resp) => {
+    if (resp.error !== undefined) {
+      throw resp;
+    }
+    await listUpcomingEvents();
+  };
+
+  if (gapi.client.getToken() === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
+    tokenClient.requestAccessToken({ prompt: "consent" });
+  } else {
+    // Skip display of account chooser and consent dialog for an existing session.
+    tokenClient.requestAccessToken({ prompt: "" });
+  }
+}
+
+async function listUpcomingEvents() {
+  let response;
+  try {
+    const request = {
+      calendarId: "primary",
+      timeMin: new Date(year, month, 1).toISOString(),
+      timeMax: new Date(year, month + 1, 0).toISOString(),
+      showDeleted: false,
+      singleEvents: true,
+      orderBy: "startTime",
+    };
+    response = await gapi.client.calendar.events.list(request);
+  } catch (err) {
+    console.log(err.message);
+    return;
+  }
+
+  const googleEvents = response.result.items;
+  if (!googleEvents || googleEvents.length == 0) {
+    // document.getElementById('content').innerText = 'No googleEvents found.';
+    return;
+  }
+
+  // Flatten to string to display
+
+  const output = googleEvents.reduce((str, event) =>
+    events.push({
+      date: new Date(
+        `${event.start.dateTime || event.start.date}`
+      ).toLocaleDateString("en-us", {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+      }),
+      title: event.summary,
+    })
+  );
+  // document.getElementById('content').innerText = output;
 }
 
 function initButtons() {
-	document.getElementById('nextButton').addEventListener('click', ()=> {
-		nav++;
-		load();
-	});
-	document.getElementById('backButton').addEventListener('click', ()=> {
-		nav--;
-		load(); 
-	});
+  document.getElementById("nextButton").addEventListener("click", () => {
+    nav++;
+    load();
+  });
+  document.getElementById("backButton").addEventListener("click", () => {
+    nav--;
+    load();
+  });
 
-	document.getElementById('saveButton').addEventListener('click', saveEvent);
-	document.getElementById('cancelButton').addEventListener('click', closeModal);
-	
-	document.getElementById('deleteButton').addEventListener('click', deleteEvent);
-	document.getElementById('closeButton').addEventListener('click', closeModal);
+  document
+    .getElementById("loginButton")
+    .addEventListener("click", handleAuthClick);
+
+  document.getElementById("saveButton").addEventListener("click", saveEvent);
+  document.getElementById("cancelButton").addEventListener("click", closeModal);
+
+  document
+    .getElementById("deleteButton")
+    .addEventListener("click", deleteEvent);
+  document.getElementById("closeButton").addEventListener("click", closeModal);
 }
 
 initButtons();
